4fb2f12de8ae8808f288478bc0626410be4d1a3b
rodillos <rbotocorbal@gmail.com>
2025-01-28 17:55:56 -0300

    Added courses list file

---
+++ courses/coursesList.js
@@ -0,0 +1,148 @@
+const CupList = [
+    {
+        cupName: 'Mushroom',
+        dlc: false,
+        icon : 'Mushroom',
+        courses: ['Mario Kart Stadium', 'Water Park', 'Sweet Sweet Canyon', 'Thwomp Ruins']
+    },
+    {
+        cupName: 'Flower',
+        dlc: false,
+        icon : 'Flower',
+        courses: ['Mario Circuit', 'Toad Harbor', 'Twisted Mansion', 'Shy Guys Falls']
+    },
+    {
+        cupName: 'Star',
+        dlc: false,
+        icon : 'Star',
+        courses: ['Sunshine Airport', 'Dolphin Shoals', 'Electrodrome', 'Monte Wario']
+    },
+    {
+        cupName: 'Special',
+        dlc: false,
+        icon : 'Crown',
+        courses: ['Cloudtop Cruise', 'Bone-dry Dunes', 'Bowers Castle', 'Rainbow Road']
+    },
+    {
+        cupName: 'Egg',
+        dlc: false,
+        icon : 'Egg',
+        courses: ['Yoshi Circuite', 'Excitebike Arena', 'Dragon Driftway', 'Mute City']
+    },
+    {
+        cupName: 'Crossing',
+        dlc: false,
+        icon : 'Green Leaf',
+        courses: ['Baby Park', 'Cheese Land', 'Wild Woods', 'Animal Corssing']
+    },
+    {
+        cupName: 'Shell',
+        dlc: false,
+        icon : 'Turtle',
+        courses: ['Moo Moo Meadows', 'Mario Circuit (GBA)', 'Cheep Cheep Beach', 'Toads Turnpike']
+    },
+    {
+        cupName: 'Banana',
+        dlc: false,
+        icon : 'Banana',
+        courses: ['Dry Dry Desert', 'Donut Plains 3', 'Royal Raceway', 'DK Jungle']
+    },
+    {
+        cupName: 'Leaf',
+        dlc: false,
+        icon : 'Brown Leaf',
+        courses: ['Wario Stadium', 'Sherbet Land', 'Music Park', 'Yoshi Valley']
+    },
+    {
+        cupName: 'Lightning',
+        dlc: false,
+        icon : 'Thunder',
+        courses: ['Tick-Tock Clock', 'Piranha Plant Slide', 'Grumble Volcano', 'Rainbow Road (N64)']
+    },
+    {
+        cupName: 'Triforce',
+        dlc: false,
+        icon : 'Triangle',
+        courses: ['Warios Gold Mine', 'Rainbvow Road (SNES)', 'Ice Ice Outpost', 'Hyrule Circuit']
+    },
+    {
+        cupName: 'Bell',
+        dlc: false,
+        icon : 'Bell',
+        courses: ['Neo Bowser City', 'Ribbon Road', 'Super Bell Subway', 'Big Blue']
+    },
+    {
+        cupName: 'Golden Dash',
+        dlc: true,
+        icon : 'Mushroom Yellow',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Lucky Cat',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Turnip',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Propeller',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Rock',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Moon',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Fruit',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Boomerang',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Feather',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Cherry',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Acorn',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    {
+        cupName: 'Spiny',
+        dlc: true,
+        icon : '',
+        courses: ['trac 1', 'trac 2', 'track 3', 'track 4']
+    },
+    ];
+    
+module.exports = CupList; // Exportación correcta
