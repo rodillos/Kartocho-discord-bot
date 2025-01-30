7f5c2c0df59d04cbbe1340742149c3088af82442
rodillos <rbotocorbal@gmail.com>
2025-01-30 01:20:50 -0300

    Se modifica el algoritmo para mezclar las pistas, como tambien la prevenecion de un numero imposible de pistas; tambien se agrega el input para solicitar un numero personalizado de pistas, con una opcion defalut (8)

--- commands/utility/courses.js
+++ commands/utility/courses.js
@@ -1,47 +1,61 @@
 const { SlashCommandBuilder } = require('discord.js');
 const CupList =  require('../../courses/coursesList.js');
+const { count } = require('console');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('pista8')
-		.setDescription('Devuelve 8 pistas al azar'),
+		.setName('pista')
+		.setDescription('Devuelve N pistas al azar')
+		.addIntegerOption(option =>
+			option
+				.setName('cuantas')
+				.setDescription('La cantidad de pistas')),
+
 	async execute(interaction) {
-		const resultados = getRandomCourse(8);
+		//falta agregar el receptor del input
+		const amount = interaction.options.getInteger('cuantas') || 8;
+
+		//TODO: evitar que salgan dos pistas de la misma copa seguidas
+		//TODO: random espejo
+
+		const resultados = getRandomCourse(amount, interaction.user.username);
 		const replyMessage = resultados
-		.map((result, index) => `Pista ${index + 1}. ${result.course} (Copa: ${result.cupName}), ${result.dlc ? 'E' : 'No e'}s del DLC`)
+		.map((result, index) => `${index + 1}. Copa: ${result.cupName} - ${result.course} ${result.dlc ? ' - DLC' : ''}`)
 		.join('\n');
 	
 		// Enviar respuesta
-		await interaction.reply(`Aquí tienes 8 pistas aleatorias:\n${replyMessage}`);
+		await interaction.reply(`Aquí tienes ${amount} pistas aleatorias:\n${replyMessage}`);
 	},
 };
 
-module.exports.pista16 = {
-	data: new SlashCommandBuilder()
-	  .setName('pista16')
-	  .setDescription('Devuelve 16 pistas al azar'),
-	async execute(interaction) {
-
-	  const resultados = getRandomCourses(16);
-
-	  const replyMessage = resultados
-		.map((result, index) => `Pista ${index + 1}. ${result.course} (Copa: ${result.cupName}), ${result.dlc ? 'E' : 'No e'}s del DLC`)
-		.join('\n');
-
-	  await interaction.reply(`Aquí tienes 16 pistas aleatorias:\n${replyMessage}`);
-	},
-  };
-
   // Función para obtener un course aleatorio junto con su cupName
-  function getRandomCourse(count) {
-	// Crear una lista de objetos que vincule cada course con su cupName
-	const courseWithCupName = CupList?.flatMap(cup => 
-	  cup.courses.map(course => ({ cupName: cup.cupName, dlc: cup.dlc, course }))
-	);
-  
+  function getRandomCourse(count, userName) {
+	let randomCourses = [];
+	let index = 0;
+
+	if(count > 48){ return [{cupName: 'Clown (25)', course: userName, dlc: true}];}
+	
+	const courseWithCupName = CupList?.flatMap(cup => {
+		index++;
+	  return cup.courses.map(course => ({ 
+		cupName: `${cup.cupName} (${index})`, 
+		dlc: cup.dlc, 
+		course }))
+	});
+
 	// Mezclar los elementos de forma aleatoria
-	const shuffledCourses = courseWithCupName.sort(() => Math.random() - 0.5);
+	const shuffledCourses = shuffle(courseWithCupName);
   
 	// Tomar los primeros `count` elementos
 	return shuffledCourses.slice(0, count);
   }
+
+  function shuffle(array) {
+	let arrayAux = [...array]; //para no modificar el original
+	for (let i = arrayAux.length - 1; i > 0; i--) {
+	  let j = Math.floor(Math.random() * (i + 1)); // Índice aleatorio entre 0 e i
+	  [arrayAux[i], arrayAux[j]] = [arrayAux[j], arrayAux[i]]; // Intercambia elementos
+	}
+	return arrayAux;
+  }
+  
